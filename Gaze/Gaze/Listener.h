d5c1c7adb9aea523b94538a0fc3f5c92cf34c572
james08m <james08major@gmail.com>
2019-02-25 20:21:09 -0500

    Start Listener class declaration

--- Gaze/Gaze/Listener.h
+++ Gaze/Gaze/Listener.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include "Logger.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+class Listener
+{
+	bool Listening;
+	Logger* Log;
+	Clock Timer;
+
+public:
+	// Listener() : Class cunstructor
+	Listener();
+
+	// Start() : Start listening
+	void Start();
+
+	// Stop() : Stop listening
+	void Stop();
+
+private:
+	// Run() : Listener main loop
+	void Run();
+
+};
